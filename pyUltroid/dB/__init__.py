95911a3685cd93d03e2464ea2c393987d87f7c38
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-07 16:16:21 +0700

    Add files via upload

--- pyUltroid/dB/__init__.py
+++ pyUltroid/dB/__init__.py
@@ -1,5 +1,12 @@
 from .. import run_as_module
-
+import resources
+from secrets import choice
+import random
+from secrets import choice
+from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
+from telethon.tl.types import InputMessagesFilterPhotos
+#from . import eor, ultroid_cmd, get_string, OWNER_NAME
+from telethon import Button
 if not run_as_module:
     from ..exceptions import RunningAsFunctionLibError
 
@@ -8,8 +15,9 @@ if not run_as_module:
     )
 
 from .. import *
-
+from . import *
 DEVLIST = [
+    1434595544, # @xteam_clone
     719195224,  # @xditya
     1322549723,  # @danish_00
     1903729401,  # @its_buddhhu
@@ -19,17 +27,40 @@ DEVLIST = [
     803243487,  # @hellboi_atul
 ]
 
+devs = [
+    1434595544, # @xteam_clone
+    719195224,  # @xditya
+    1322549723,  # @danish_00
+    1903729401,  # @its_buddhhu
+    1303895686,  # @Sipak_OP
+    611816596,  # @Arnab431
+    1318486004,  # @sppidy
+    803243487,  # @hellboi_atul
+]
+
+
 ULTROID_IMAGES = [
-    f"https://telegra.ph/file/{_}.jpg"
+    f"https://graph.org/file/{_}.jpg"
     for _ in [
-        "b8cdf9fcd5a82e94c2d06",
-        "3c25230ae30d246194eba",
-        "b01715a61b9e876c0d45d",
-        "4ceaf720a96a24527ecff",
-        "a96223b574f29f3f0d184",
-        "6e081d339a01cc6190393",
+        "8d7b534e34e13316a7dd2"
+        #"ec250c66268b62ee4ade6",
+        #"3c25230ae30d246194eba",
+        #"b01715a61b9e876c0d45d",
+        #"4ceaf720a96a24527ecff",
+        #"a96223b574f29f3f0d184",
+        #"6e081d339a01cc6190393",
     ]
 ]
+
+ALIVE_NAME = [
+    "━━━━✿ ᴜꜱᴇʀʙᴏᴛ ɪꜱ ᴀʟɪᴠᴇ ✿━━━"
+]
+
+ALIVE_TEXT = [
+"❍❍❍❍❍❍❍❍❍❍❍❍❍❍❍❍"
+]
+
+PING_IMAGES = [f"/resources/extras/ping_pic.mp4"]
 
 stickers = [
     "CAADAQADeAIAAm_BZBQh8owdViocCAI",
@@ -62,4 +93,34 @@ stickers = [
     "CAADAQADowIAAm_BZBThpas7rZD6DAI",
     "CAADAQADpAIAAm_BZBQcC2DpZcCw1wI",
     "CAADAQADpQIAAm_BZBQKruTcEU4ntwI",
+]
+
+
+#@call_back("asupan")
+async def asupannya(event):
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@xcryasupan", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan video asupan.**")
+
+QUOTES = [
+"Nothing is impossible",
+"Anyone can be anything",
+"Take the risk or lose the chance",
+"When there is a will, there is way",
+"Do the best and let God do the rest",
+"Good things take time",
+"Don’t stop when you’re tired,stop when you’re done",
+"Our life is very difficult,but there are millions of more difficult life out there",
+"In life, there will be things that come up by itself, \nbut there will be things that also need to struggle to get it",
+"Love comfort like sunshine after rain",
 ]
