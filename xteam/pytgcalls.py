50fbfa75435249c2e27f99cdc5600039bb236278
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-14 22:11:58 +0700

    Update pytgcalls.py

--- xteam/pytgcalls.py
+++ xteam/pytgcalls.py
@@ -17,7 +17,7 @@ ASSISTANT_ID = Var.ASSISTANT_ID
 
 bot = TelegramClient('Zaid', api_id=Var.API_ID, api_hash=Var.API_HASH)
 Zaid = bot.start(bot_token=Var.BOT_TOKEN)
-client = TelegramClient(StringSession(session), Var.API_ID, Var.API_HASH)
+client = TelegramClient(StringSession(Var.SESSION), Var.API_ID, Var.API_HASH)
 call_py = PyTgCalls(client)
 client.start()
 call_py.start()
