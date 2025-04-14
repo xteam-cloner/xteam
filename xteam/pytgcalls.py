67eba579effa4fbab743a1fdc270ebab29823def
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-14 21:57:32 +0700

    Create pytgcalls.py

---
+++ xteam/pytgcalls.py
@@ -0,0 +1,24 @@
+import os
+
+from telethon import TelegramClient
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+import logging
+from pytgcalls import PyTgCalls
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+
+from .configs import Var
+BOT_USERNAME = Var.BOT_USERNAME
+ASSISTANT_ID = Var.ASSISTANT_ID
+
+bot = TelegramClient('Zaid', api_id=Var.API_ID, api_hash=Var.API_HASH)
+Zaid = bot.start(bot_token=Var.BOT_TOKEN)
+client = TelegramClient(StringSession(Var.SESSION), Var.API_ID, Var.API_HASH)
+call_py = PyTgCalls(client)
+client.start()
+call_py.start()
