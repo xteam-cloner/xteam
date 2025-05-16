f2af107125b50fc86295701130f01e1f5d2f1b92
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-16 14:33:10 +0700

    Update __init__.py

--- xteam/dB/__init__.py
+++ xteam/dB/__init__.py
@@ -16,6 +16,7 @@ if not run_as_module:
 
 from .. import *
 from . import *
+
 DEVLIST = [
     1434595544, # @xteam_clone
     719195224,  # @xditya
@@ -39,7 +40,7 @@ devs = [
 ]
 
 
-"""ULTROID_IMAGES = [
+ULTROID_IMAGES = [
     f"https://graph.org/file/{_}.jpg"
     for _ in [
         "8d7b534e34e13316a7dd2"
@@ -49,13 +50,6 @@ devs = [
         #"4ceaf720a96a24527ecff",
         #"a96223b574f29f3f0d184",
         #"6e081d339a01cc6190393",
-    ]
-]"""
-
-ULTROID_IMAGES = [
-    f"https://files.catbox.moe/{_}.jpg"
-    for _ in [
-        "k9ljs"
     ]
 ]
 
