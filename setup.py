5a3d1c1d3d04f0badf02df07b10d8f558b4c423d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-10-11 10:35:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import re
 
 import setuptools
 
-requirements = ["redis", "hiredis", "python-decouple", "python-dotenv"]
+requirements = ["pymongo", "python-decouple", "python-dotenv"]
 
 with open("xteam/version.py", "rt", encoding="utf8") as x:
     version = re.search(r'__version__ = "(.*?)"', x.read()).group(1)
