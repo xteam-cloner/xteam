1f7074abb10d91136188f5c594eb5f53b5b275ef
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-05 10:17:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,61 +1,8 @@
-import re
+Import re
+
 import setuptools
 
-requirements = [
-    "telethon-patch",
-    "aiofiles==24.1.0",
-    "aiohttp==3.10.3",
-    "asyncio==3.4.3",
-    "httpx==0.25.0",
-    "motor==3.6.0",
-    "numpy==2.2.5",
-    "pillow",
-    "pydantic",
-    "py-tgcalls==2.2.8",
-    "python-dotenv==1.0.1",
-    "python-decouple",
-    "youtube-dl",
-    "youtube-search==2.1.0",
-    "kurigram",
-    "ffmpeg",
-    "python-dateutil",
-    "deep-translator",
-    "speedtest-cli",
-    "python-telegram-bot",
-    "emoji",
-    "tqdm",
-    "pyshorteners",
-    "apscheduler",
-    "bs4",
-    "enhancer>=0.3.4",
-    "gitpython",
-    "google-api-python-client",
-    "htmlwebshot",
-    "lottie",
-    "lxml",
-    "numpy>=1.21.2",
-    "oauth2client",
-    "opencv-python-headless",
-    "pillow>=9.0.0",
-    "profanitydetector",
-    "psutil",
-    "pypdf2>=1.26.0",
-    "pytz",
-    "gtts",
-    "qrcode",
-    "requests",
-    "tabulate",
-    "telegraph",
-    "tgcrypto",
-    "youtube-search-python",
-    "yt-dlp",
-    "flask",
-    "flask_restful",
-    "wget",
-    "catbox-uploader",
-    "cloudscraper",
-    "pymongo",
-]
+requirements = ["pymongo", "python-decouple", "python-dotenv"]
 
 with open("xteam/version.py", "rt", encoding="utf8") as x:
     version = re.search(r'__version__ = "(.*?)"', x.read()).group(1)
@@ -64,14 +11,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 name = "xteam"
-author = "TeamX"
+author = "xteam_cloner"
 author_email = "xteamji@gmail.com"
-description = "A Secure and Powerful Python-Telethon Based Library For xteam Userbot."
+description = "A Secure and Powerful Python-Telethon Based Library For xteam-urbot."
 license_ = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
 url = "https://github.com/xteam-cloner/xteam"
 project_urls = {
     "Bug Tracker": "https://github.com/xteam-cloner/xteam/issues",
-    "Documentation": "https://ultroid.tech",
+    "Documentation": "https://t.me/xteam_cloner",
     "Source Code": "https://github.com/xteam-cloner/xteam",
 }
 classifiers = [
@@ -81,6 +28,7 @@ classifiers = [
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
 ]
