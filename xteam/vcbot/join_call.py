a3f5706e0239825f70ccb4f55a9972bf218214b8
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-15 00:12:44 +0700

    Update join_call.py

--- xteam/vcbot/join_call.py
+++ xteam/vcbot/join_call.py
@@ -12,8 +12,8 @@ FILE_PATH = os.path.join(os.getcwd(), 'resources', 'audio-man.mp3')
 async def join_call(chat_id: int, link: str, video: bool = False, resolution: int = 480):
     
     try:
-        await join_call(chat_id, link=FILE_PATH, video=False).start() 
-        LOGS.info(f"Successfully started/joined VC in {chat_id} using get_group_call().start().")
+        await join_call(chat_id, link=FILE_PATH, video=False)
+        LOGS.info(f"Successfully joined VC in {chat_id} ")
         
     except UserAlreadyParticipantError:
         LOGS.warning(f"Assistant already in VC in {chat_id}. Proceeding to play.")
