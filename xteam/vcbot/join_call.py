c131c374b8c8dba46e0458cd89a4381e74fa84e5
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-14 23:43:32 +0700

    Update join_call.py

--- xteam/vcbot/join_call.py
+++ xteam/vcbot/join_call.py
@@ -7,14 +7,14 @@ from xteam import call_py, LOGS
 async def join_call(chat_id: int, link: str, video: bool = False, resolution: int = 480):
     
     try:
-        await call_py.join_call(chat_id)
-        LOGS.info(f"Successfully joined VC in {chat_id}.")
+        await call_py.get_group_call(chat_id).start() 
+        LOGS.info(f"Successfully started/joined VC in {chat_id} using get_group_call().start().")
         
     except UserAlreadyParticipantError:
         LOGS.warning(f"Assistant already in VC in {chat_id}. Proceeding to play.")
         
     except NoActiveGroupCall:
-        LOGS.warning(f"No active VC in {chat_id}. Cannot start playback.")
+        LOGS.warning(f"No active VC in {chat_id} found by PyTgCalls.")
         return 0
         
     except Exception as e:
@@ -25,7 +25,6 @@ async def join_call(chat_id: int, link: str, video: bool = False, resolution: in
         
         if video:
             video_params = VideoQuality.HD_720p if resolution >= 720 else VideoQuality.SD_480p 
-            
             stream = MediaStream(
                 media_path=link, 
                 audio_parameters=AudioQuality.HIGH,
@@ -38,10 +37,7 @@ async def join_call(chat_id: int, link: str, video: bool = False, resolution: in
                 video_flags=MediaStream.Flags.IGNORE, 
             )
             
-        await call_py.play(
-            chat_id,
-            stream,
-        )
+        await call_py.play(chat_id, stream)
         
         LOGS.info(f"Started playback in {chat_id} successfully. Link: {link}")
         return 1
