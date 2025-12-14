98897753634b918a1c4e38ffa86a353ab9a521a8
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-14 16:51:54 +0700

    Update __init__.py

--- xteam/vcbot/__init__.py
+++ xteam/vcbot/__init__.py
@@ -2,5 +2,5 @@ from .chattitle import CHAT_TITLE
 from .controls import skip_current_song, skip_item, play_next_stream
 from .queues import QUEUE, add_to_queue, clear_queue, get_queue, pop_an_item
 from .thumbnail import gen_thumb
-from .ytdl import ytdl, ytsearch
+from .ytdl import ytsearch
 from .join_call import join_call
