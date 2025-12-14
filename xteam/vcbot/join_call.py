94c09e1782be51ef2a7ac5b39293e96ef1ef5232
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-15 00:07:05 +0700

    Update join_call.py

--- xteam/vcbot/join_call.py
+++ xteam/vcbot/join_call.py
@@ -4,10 +4,13 @@ from pytgcalls.types import MediaStream
 from pytgcalls.types.stream import VideoQuality, AudioQuality
 from xteam import call_py, LOGS 
 
+FILE_PATH = os.path.join(os.getcwd(), 'resources', 'audio-man.mp3') 
+
+
 async def join_call(chat_id: int, link: str, video: bool = False, resolution: int = 480):
     
     try:
-        await join_call(chat_id).start() 
+        await join_call(chat_id, link=FILE_PATH, video=True).start() 
         LOGS.info(f"Successfully started/joined VC in {chat_id} using get_group_call().start().")
         
     except UserAlreadyParticipantError:
