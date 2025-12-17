23e9f44b01d1c23f3fcaf49f0fc5539d29a7292d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-18 00:35:00 +0700

    Update queues.py

--- xteam/vcbot/queues.py
+++ xteam/vcbot/queues.py
@@ -3,38 +3,19 @@ import contextlib
 
 QUEUE = {}
 
-
 def add_to_queue(chat_id, songname, link, ref, type, quality):
     if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        chat_queue.append([songname, link, ref, type, quality])
-        return int(len(chat_queue) - 1)
-    QUEUE[chat_id] = [[songname, link, ref, type, quality]]
-
+        QUEUE[chat_id].append([songname, link, ref, type, quality])
+        return int(len(QUEUE[chat_id]) - 1)
+    else:
+        QUEUE[chat_id] = [[songname, link, ref, type, quality]]
+        return 0
 
 def get_queue(chat_id):
-    if chat_id in QUEUE:
-        return QUEUE[chat_id]
-    return []
-
-
-
-def pop_an_item(chat_id: int):
-    if chat_id not in QUEUE or not QUEUE[chat_id]:
-        return
-        
-    song_data = QUEUE[chat_id][0]
-    file_path = song_data[1]
-
-    try:
-        QUEUE[chat_id].pop(0)
-    except IndexError:
-        pass
-        
+    return QUEUE.get(chat_id, [])
 
 def clear_queue(chat_id: int):
-    if chat_id not in QUEUE:
-        return 0
-    QUEUE.pop(chat_id)
-    return 1
-
+    if chat_id in QUEUE:
+        QUEUE.pop(chat_id)
+        return 1
+    return 0
