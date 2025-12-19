18c5dc80dc3d2a0f7f255e802310ee819a62354c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-19 14:53:39 +0700

    Update queues.py

--- xteam/vcbot/queues.py
+++ xteam/vcbot/queues.py
@@ -7,33 +7,39 @@
 
 QUEUE = {}
 
-
-def add_to_queue(chat_id, songname, link, ref, type, quality):
-    if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        chat_queue.append([songname, link, ref, type, quality])
-        return int(len(chat_queue) - 1)
-    QUEUE[chat_id] = [[songname, link, ref, type, quality]]
-
+def add_to_queue(chat_id, songname, url, duration, thumb_url, videoid, artist):
+    if chat_id not in QUEUE:
+        QUEUE[chat_id] = []
+    
+    data = [
+        songname, 
+        url, 
+        duration, 
+        thumb_url, 
+        videoid, 
+        artist
+    ]
+    
+    QUEUE[chat_id].append(data)
+    return len(QUEUE[chat_id]) - 1
 
 def get_queue(chat_id):
     if chat_id in QUEUE:
         return QUEUE[chat_id]
     return 0
 
-
 def pop_an_item(chat_id):
     if chat_id not in QUEUE:
         return 0
-
     chat_queue = QUEUE[chat_id]
-    chat_queue.pop(0)
-    return 1
-
+    if chat_queue:
+        chat_queue.pop(0)
+        return 1
+    return 0
 
 def clear_queue(chat_id: int):
-    if chat_id not in QUEUE:
-        return 0
-
-    QUEUE.pop(chat_id)
-    return 1
+    if chat_id in QUEUE:
+        QUEUE.pop(chat_id)
+        return 1
+    return 0
+    
