40b842bfe497617dae0bb5d8994289e2eb0dd5b5
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-20 00:17:59 +0700

    Add MUSIC_BUTTONS to imports in __init__.py

--- xteam/vcbot/__init__.py
+++ xteam/vcbot/__init__.py
@@ -4,4 +4,4 @@ from .queues import QUEUE, add_to_queue, clear_queue, get_queue, pop_an_item
 from .thumbnail import gen_thumb, get_play_text, get_play_queue
 from .ytdl import ytdl, ytsearch
 from .join_call import join_call
-from .markups import telegram_markup_timer
+from .markups import telegram_markup_timer, MUSIC_BUTTONS
