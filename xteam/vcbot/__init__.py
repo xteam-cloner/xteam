cff6e3d45c5528b6ea3555a7b6f2b29c94ec3ee7
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-01-20 10:41:59 +0700

    Update __init__.py

--- xteam/vcbot/__init__.py
+++ xteam/vcbot/__init__.py
@@ -5,3 +5,4 @@ from .thumbnail import gen_thumb, get_play_text, get_play_queue
 from .ytdl import ytdl, ytsearch
 from .join_call import join_call
 from .markups import telegram_markup_timer, MUSIC_BUTTONS
+from .joiner import AssistantAdd
