32b641a54bae90ba5333736707667aaec39aaec4
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-01-20 11:07:15 +0700

    Fix import path for Var in joiner.py

--- xteam/vcbot/joiner.py
+++ xteam/vcbot/joiner.py
@@ -2,7 +2,7 @@ from typing import *
 import random
 from typing import Dict, List, Union
 
-from .configs import Var
+from xteam.configs import Var
 from telethon import *
 from telethon.errors.rpcerrorlist import (
     UserAlreadyParticipantError,
