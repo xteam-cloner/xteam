a03175bcd85a9f354733c7b84eda6ed3f28b8b64
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-01-21 20:55:40 +0700

    Import cleanup_file from ytdl module

--- xteam/vcbot/__init__.py
+++ xteam/vcbot/__init__.py
@@ -2,7 +2,7 @@ from .chattitle import CHAT_TITLE
 from .controls import skip_current_song, skip_item
 from .queues import QUEUE, add_to_queue, clear_queue, get_queue, pop_an_item
 from .thumbnail import gen_thumb, get_play_text, get_play_queue
-from .ytdl import ytdl, ytsearch
+from .ytdl import ytdl, ytsearch, cleanup_file
 from .join_call import join_call
 from .markups import telegram_markup_timer, MUSIC_BUTTONS
 from .joiner import AssistantAdd
