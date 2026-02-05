afce6e1ae74eb2e7546084f9a901d18ecf2e0cd7
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-02-05 13:11:55 +0700

    Update ytdl.py

--- xteam/vcbot/ytdl.py
+++ xteam/vcbot/ytdl.py
@@ -14,7 +14,6 @@ COOKIES_FILE_PATH = "cookies.txt"
 if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
 
-
 def ytsearch(query: str) -> Union[List[Any], int]:
     try:
         search = VideosSearch(query, limit=1).result()
@@ -34,26 +33,24 @@ async def ytdl(url: str, video_mode: bool = False) -> Tuple[int, str]:
             "nocheckcertificate": True,
             "cookiefile": COOKIES_FILE_PATH if os.path.exists(COOKIES_FILE_PATH) else None,
             "outtmpl": f"{DOWNLOAD_DIR}/%(id)s.%(ext)s",
+            "noplaylist": True,
         }
+        
         if video_mode:
-            opts["format"] = "bestvideo[height<=?720][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]"
-            target_ext = "mp4"
+            opts["format"] = "bestvideo[height<=720]+bestaudio/best[height<=720]"
         else:
-            opts["format"] = "bestaudio[ext=m4a]/bestaudio"
-            target_ext = "m4a"
+            opts["format"] = "bestaudio/best"
 
         with yt_dlp.YoutubeDL(opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            v_id = info['id']
-            return os.path.abspath(os.path.join(DOWNLOAD_DIR, f"{v_id}.{target_ext}"))
+            return ydl.prepare_filename(info)
 
     try:
         path = await loop.run_in_executor(None, download_sync)
-        return 1, path
+        return 1, os.path.abspath(path)
     except Exception as e:
         return 0, str(e)
 
-            
 async def get_playlist_ids(link, limit):
     command = f"yt-dlp -i --get-id --flat-playlist --playlist-end {limit} --skip-download '{link}'"
     process = await asyncio.create_subprocess_shell(
@@ -74,3 +71,4 @@ async def cleanup_file(filepath: str, delay: int = 1800):
             os.remove(filepath)
     except:
         pass
+    
