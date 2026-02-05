78354eb854686217f7292069a79fb5df7b2e9ee9
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2026-02-05 21:20:26 +0700

    Update join_call.py

--- xteam/vcbot/join_call.py
+++ xteam/vcbot/join_call.py
@@ -11,10 +11,12 @@ from ntgcalls import TelegramServerError, ConnectionNotFound
 
 from pytgcalls.types import MediaStream
 from pytgcalls.types.stream import VideoQuality, AudioQuality
-from xteam import call_py, LOGS, asst
-from xteam.configs import Var
+from xteam import call_py, LOGS, bot
 
-async def join_call(chat_id: int, link: str, title: str, duration: str, chat_name: str, video: bool = False):
+FILE_PATH = os.path.join(os.getcwd(), 'resources', 'audio-man.mp3') 
+
+async def join_call(chat_id: int, link: str, video: bool = False):
+    
     try:
         if video:
             stream = MediaStream(
@@ -34,20 +36,7 @@ async def join_call(chat_id: int, link: str, title: str, duration: str, chat_nam
 
     try:
         await call_py.play(chat_id, stream)
-        LOGS.info(f"Berhasil memulai playback di VC {chat_id}. Link: {link}")
-        
-        if Var.LOG_CHANNEL:
-            text = (
-                "<blockquote>"
-                "🎵 Music Started\n\n"
-                f"📌 Title: <code>{title}</code>\n"
-                f"🕒 Duration: <code>{duration}</code>\n"
-                f"👥 Group: <code>{chat_name}</code>\n"
-                f"🆔 ID: <code>{chat_id}</code>"
-                "</blockquote>"
-            )
-            await asst.send_message(Var.LOG_CHANNEL, text, parse_mode='html')
-            
+        LOGS.info(f"Playing now!")
         return 1
         
     except NoActiveGroupCall:
@@ -68,11 +57,12 @@ async def join_call(chat_id: int, link: str, title: str, duration: str, chat_nam
         
     except Exception as e:
         error_msg = str(e).lower()
+        
         if 'already' in error_msg:
              LOGS.warning(f"Asisten sudah ada di VC {chat_id}. Mencoba mengganti stream...")
              try:
                  await call_py.change_stream(chat_id, stream)
-                 LOGS.info(f"Stream berhasil diperbarui di {chat_id}.")
+                 LOGS.info(f"Stream berhasil diperbarui ke FHD di {chat_id}.")
                  return 1
              except Exception as sub_e:
                  LOGS.error(f"Gagal mengganti stream di VC {chat_id}: {sub_e}")
